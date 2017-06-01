5474d3e476937a46a52e58877587ff8572bc20fb
Andrew Li <al2114@ic.ac.uk>
2017-06-01 19:36:17 +0100

    added voice recognition

---
+++ Hey Coacher/hey-coacher/DelegateFunctions.swift
@@ -0,0 +1,15 @@
+//
+//  RootDelegateFunctions.swift
+//  test-structure
+//
+//  Created by Andrew Li on 01/06/2017.
+//  Copyright © 2017 imperial-smartbike. All rights 
+
+
+extension RootViewController {
+  
+  func transitionTo(viewId: String){
+    print("Root delegated, switching container view")
+    containerSwitchToViewController(viewId)
+  }
+}
