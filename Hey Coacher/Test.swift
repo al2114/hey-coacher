3be1fd75df0919a79d663b6ced9cac23735da58b
Andrew Li <al2114@ic.ac.uk>
2017-06-01 16:34:10 +0100

    test commit

---
+++ Hey Coacher/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  Hey Coacher
+//
+//  Created by Andrew Li on 01/06/2017.
+//  Copyright © 2017 imperial-smartbike. All rights reserved.
+//
+
+import Foundation
+
+funct test(){
+  print("Hello world!")
+}
